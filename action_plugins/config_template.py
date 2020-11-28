c23cef41e1644f688d0a0a2967f568122462697d
Mario Lubenka <mario.lubenka@googlemail.com>
2020-11-28 23:30:25 +0100

    fix: remove unknown original_basename setting

--- action_plugins/config_template.py
+++ action_plugins/config_template.py
@@ -635,7 +635,6 @@ class ActionModule(ActionBase):
             dict(
                 src=transferred_data,
                 dest=_vars['dest'],
-                original_basename=os.path.basename(source),
                 follow=True,
             ),
         )
